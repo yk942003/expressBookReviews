cbc7357bb66b2c9563ae733906d57d16f8b835fd
Yong Kit <youremail@example.com>
2026-01-27 23:38:47 +0800

    Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  Object.values(books).forEach(book => {
+    if (book.author === author) {
+        res.send(JSON.stringify(book));
+    }
+  });
+  res.status(404).json({message: "Author not found"});
 });
 
 // Get all books based on title
