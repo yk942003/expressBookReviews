4c3cab7b70961adca873217b8b5ab04b3aded347
Yong Kit <youremail@example.com>
2026-01-27 23:50:52 +0800

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,7 +53,8 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn].reviews));
 });
 
 module.exports.general = public_users;
