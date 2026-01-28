cec10a72bc5c22405e50580b29e4ce46fc6d2d71
Yong Kit <youremail@example.com>
2026-01-29 01:02:40 +0800

    Task 12

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -63,22 +63,44 @@ public_users.get('/isbn/:isbn',function (req, res) {
             res.send(JSON.stringify(result));
         })
         .catch((err) => {
-            return res.status(500).json({ message: "Error retrieving book"})
-        })
+            return res.status(404).json({ message: "Error retrieving book from isbn"})
+        });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const author = req.params.author;
-  const results = Object.values(books).filter(
-    book => book.author === author
-  );
+//   const author = req.params.author;
+//   const results = Object.values(books).filter(
+//     book => book.author === author
+//   );
 
-  if (results.length === 0) {
-    return res.status(404).json({message: "Author not found"});
-  }
-  res.send(JSON.stringify(results));
+//   if (results.length === 0) {
+//     return res.status(404).json({message: "Author not found"});
+//   }
+//   res.send(JSON.stringify(results));
+
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const author = req.params.author;
+            const results = Object.values(books).filter(
+                book => book.author === author
+            );
+            resolve(results);
+        }, 5000);
+    });
+
+    myPromise
+        .then((result) => {
+            if (result.length === 0) {
+                return res.status(404).json({ message: "Author not found" });
+            }
+            return res.status(200).json(JSON.stringify(result));
+        })
+        .catch((err) => {
+            return res.status(404).json({ message: "Error retrieving book from author"})
+        });
+
 });
 
 // Get all books based on title
