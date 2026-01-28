6d444e6e8c2bd1663c5605f7f3cffc47217850be
Yong Kit <youremail@example.com>
2026-01-29 00:49:28 +0800

    Task 10

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,7 +28,21 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books));
+    // res.send(JSON.stringify(books));
+    let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(books);
+        }, 5000);
+    });
+
+    myPromise
+        .then((result) => {
+            return res.status(200).json(JSON.stringify(books));
+        })
+        .catch((err) => {
+            return res.status(500).json({ message: "Error retrieving book list"})
+        });
+
 });
 
 // Get book details based on ISBN
